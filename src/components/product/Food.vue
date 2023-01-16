968621b765e7da1bdf84588d9e1f6bfeaedc7bb4
maede kaboli <kabolimaede@gmail.com>
2023-01-16 16:57:42 +0330

    pizger project has been started

---
+++ src/components/product/Food.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import FoodType from './FoodType'
+
+defineProps<{
+    product: FoodType
+}>()
+
+const show = ref(false)
+</script>
+
+
+<template>
+    <v-col cols="md-4 Product">
+        <v-card class="mx-auto">
+            <v-img :src="`https://api.lorem.space/image/${product.id % 2 === 0 ? 'pizza' : 'burger'}?w=365&h=200&hash=${product.id}`
+            " height="200px" cover></v-img>
+
+            <v-card-title class="pb-0">
+                {{ product.title }}
+            </v-card-title>
+            <v-card-subtitle>{{ product.brand }}</v-card-subtitle>
+            <v-chip class="mx-2 mt-6" color="indigo">
+                <v-icon start icon="mdi-currency-usd"></v-icon>
+                {{ product.price }}
+            </v-chip>
+
+            <v-card-actions>
+                <v-btn color="blue" variant="outlined" size="x-small" icon="mdi-pencil-outline"></v-btn>
+                <v-btn color="red" variant="outlined" size="x-small" icon="mdi-trash-can-outline"></v-btn>
+
+                <v-spacer></v-spacer>
+
+                <v-btn :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show = !show"></v-btn>
+            </v-card-actions>
+
+            <v-expand-transition>
+                <div v-show="show">
+                    <v-divider></v-divider>
+                    <div class="mt-6 mx-2">
+                        <v-chip class="mr-1 mb-2" v-for="i in 10" :key="i" variant="elevated" color="#546E7A" size="x-small">
+                            Chip and khiar
+                        </v-chip>
+                    </div>
+                    <v-card-text>
+                        {{ product.description }}
+                    </v-card-text>
+                </div>
+            </v-expand-transition>
+        </v-card>
+    </v-col>
+</template>
+
+
+<style>
+.v-card .v-card-title {
+    line-height: 23px;
+}
+
+.v-card .v-card-subtitle {
+    font-size: 0.775rem;
+}
+</style>
---
+++ src/components/product/FoodList.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import axios from '../../api'
+import { ref, defineAsyncComponent } from 'vue'
+import FoodType from './FoodType';
+const Product = defineAsyncComponent(() => import('./Food.vue'));
+
+const snackbar = ref<boolean>(false)
+const loaded = ref<boolean>(false)
+const loading = ref<boolean>(false)
+const snackbarMsg = ref<null | string>(null)
+
+const products = ref<FoodType[]>([])
+await axios.get('products').then(res => {
+  products.value = res?.data?.products
+}).catch(err => {
+  snackbar.value = true
+  snackbarMsg.value = err
+})
+
+const onClick = () => {
+  loading.value = true
+
+  setTimeout(() => {
+    loading.value = false
+    loaded.value = true
+  }, 2000)
+}
+
+
+</script>
+
+<template>
+  <v-row class="fill-height mb-10" align-content="center" justify="center">
+
+    <v-col cols="md-6">
+      <v-text-field :loading="loading" density="compact" variant="solo" label="Search templates"
+        append-inner-icon="mdi-magnify" single-line hide-details @click:append-inner="onClick"></v-text-field>
+    </v-col>
+    <v-col cols="md-6">
+      <v-btn-toggle borderless color="deep-purple-accent-3">
+        <v-btn value="left">
+          <span class="hidden-sm-and-down">Pizza</span>
+
+          <v-icon end>
+            mdi-pizza
+          </v-icon>
+        </v-btn>
+
+        <v-btn value="center">
+          <span class="hidden-sm-and-down">All</span>
+
+          <v-icon end>
+            mdi-clipboard-list-outline
+          </v-icon>
+        </v-btn>
+
+        <v-btn value="right">
+          <span class="hidden-sm-and-down">Burger</span>
+
+          <v-icon end>
+            mdi-hamburger
+          </v-icon>
+        </v-btn>
+      </v-btn-toggle>
+    </v-col>
+  </v-row>
+  <div class="d-flex flex-row flex-wrap">
+    <Product v-for="product in products" :key="product.id" :product="product">
+    </Product>
+  </div>
+  <v-snackbar v-model="snackbar" :timeout="5000" color="red accent-1" location="top">
+    {{ snackbarMsg }}
+  </v-snackbar>
+</template>
+
