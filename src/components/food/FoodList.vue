4ee3796eb3b2dc0370b4fa1e2da9ff90ab4c5ac2
maede kaboli <kabolimaede@gmail.com>
2023-01-18 18:53:47 +0330

    toggle btns changed base on v-for in foodList

--- src/components/food/FoodList.vue
+++ src/components/food/FoodList.vue
@@ -2,6 +2,7 @@
 import { ref, defineAsyncComponent } from 'vue'
 import useFoodsListStore from '../../stores/FoodsListStore'
 import { storeToRefs } from 'pinia'
+import SelectedFoodType from './selectedFoodType'
 
 const Food = defineAsyncComponent(() => import('./Food.vue'))
 const { foodsList } = storeToRefs(useFoodsListStore())
@@ -24,39 +25,27 @@ const onClick = () => {
     loaded.value = true
   }, 2000)
 }
+
+const foods = ref<SelectedFoodType[]>([
+  { value: 1, name: 'Pizza', icon: 'mdi-pizza', color: 'red' },
+  { value: 0, name: 'All', icon: 'mdi-clipboard-list-outline', color: 'black' },
+  { value: 2, name: 'Burger', icon: 'mdi-hamburger', color: '#FFAB00' }
+])
+const selectedFood = ref<SelectedFoodType>({ value: 0, name: 'All', icon: 'mdi-clipboard-list-outline', color: 'black' })
 </script>
 
 <template>
   <v-row class="fill-height mb-10" align-content="center" justify="center">
-
     <v-col cols="md-6" sm="8">
       <v-text-field v-model="query" :loading="loading" density="compact" variant="solo" label="Search"
         append-inner-icon="mdi-magnify" single-line hide-details @click:append-inner="onClick"></v-text-field>
     </v-col>
     <v-col cols="md-6" sm="4">
-      <v-btn-toggle elevation="1" borderless mandatory rounded="xl" color="deep-purple-accent-3">
-        <v-btn value="left">
-          <span class="hidden-sm-and-down">Pizza</span>
-
-          <v-icon color="red" end>
-            mdi-pizza
-          </v-icon>
-        </v-btn>
-
-        <v-btn value="center">
-          <span class="hidden-sm-and-down">All</span>
-
-          <v-icon end>
-            mdi-clipboard-list-outline
-          </v-icon>
-        </v-btn>
-
-        <v-btn value="right">
-          <span class="hidden-sm-and-down">Burger</span>
-
-          <v-icon color="#FFAB00
-" end>
-            mdi-hamburger
+      <v-btn-toggle elevation="1" rounded="xl" v-model="selectedFood" mandatory borderless color="deep-purple-accent-3">
+        <v-btn :value="n" v-for="n in foods" :key="n">
+          <span class="hidden-sm-and-down mr-2">{{ n.name }}</span>
+          <v-icon :color="n.color" center>
+            {{ n.icon }}
           </v-icon>
         </v-btn>
       </v-btn-toggle>
