88a8fe4806919d2299013c295172b1afa6955c08
maede kaboli <kabolimaede@gmail.com>
2023-01-21 12:35:03 +0330

    not found page is responsive

--- src/layout/NotFound.vue
+++ src/layout/NotFound.vue
@@ -8,12 +8,12 @@
                             <v-img src="/not-found-burger.jpg" cover>
                             </v-img>
                         </v-col>
-                        <v-col cols="md-4">
+                        <v-col cols="md-4" sm="6">
                             <v-row>
-                                <v-col cols="md-12" class="d-flex justify-center">
+                                <v-col cols="12" class="d-flex justify-center">
                                     <h3>Page not found!</h3>
                                 </v-col>
-                                <v-col cols="md-12" class="d-flex justify-center">
+                                <v-col cols="12" class="d-flex justify-center">
                                     <router-link to="/">
                                         <v-btn variant="outlined" rounded="pill">
                                             go home
