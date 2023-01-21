8289a70fa482e38f74e83d97f6299413088ed15e
maede kaboli <kabolimaede@gmail.com>
2023-01-21 12:46:48 +0330

    empty food component added

--- src/components/food/FoodList.vue
+++ src/components/food/FoodList.vue
@@ -5,6 +5,8 @@ import { storeToRefs } from 'pinia'
 import SelectedFoodType from './selectedFoodType'
 
 const Food = defineAsyncComponent(() => import('./Food.vue'))
+const NoFood = defineAsyncComponent(() => import('./NoFood.vue'))
+
 const { foodsList } = storeToRefs(useFoodsListStore())
 const { getFoodsList } = useFoodsListStore()
 const snackbar = ref(false)
@@ -35,26 +37,30 @@ const selectedFood = ref<SelectedFoodType>({ value: 0, name: 'All', icon: 'mdi-c
 </script>
 
 <template>
-  <v-row class="fill-height mb-10" align-content="center" justify="center">
-    <v-col cols="md-6" sm="8">
-      <v-text-field v-model="query" :loading="loading" density="compact" variant="solo" label="Search"
-        append-inner-icon="mdi-magnify" single-line hide-details @click:append-inner="onClick"></v-text-field>
-    </v-col>
-    <v-col cols="md-6" sm="4">
-      <v-btn-toggle elevation="1" rounded="xl" v-model="selectedFood" mandatory borderless color="deep-purple-accent-3">
-        <v-btn :value="n" v-for="n in foods" :key="n">
-          <span class="hidden-sm-and-down mr-2">{{ n.name }}</span>
-          <v-icon :color="n.color" center>
-            {{ n.icon }}
-          </v-icon>
-        </v-btn>
-      </v-btn-toggle>
-    </v-col>
-  </v-row>
-  <div class="d-flex flex-row flex-wrap">
-    <Food v-for="food in foodsList" :key="food.id" :food="food">
-    </Food>
-  </div>
+  <NoFood v-if="foodsList.length == 0"></NoFood>
+  <template v-else>
+    <v-row class="fill-height mb-10" align-content="center" justify="center">
+      <v-col cols="md-6" sm="8">
+        <v-text-field v-model="query" :loading="loading" density="compact" variant="solo" label="Search"
+          append-inner-icon="mdi-magnify" single-line hide-details @click:append-inner="onClick"></v-text-field>
+      </v-col>
+      <v-col cols="md-6" sm="4">
+        <v-btn-toggle elevation="1" rounded="xl" v-model="selectedFood" mandatory borderless
+          color="deep-purple-accent-3">
+          <v-btn :value="n" v-for="n in foods" :key="n">
+            <span class="hidden-sm-and-down mr-2">{{ n.name }}</span>
+            <v-icon :color="n.color" center>
+              {{ n.icon }}
+            </v-icon>
+          </v-btn>
+        </v-btn-toggle>
+      </v-col>
+    </v-row>
+    <div class="d-flex flex-row flex-wrap">
+      <Food v-for="food in foodsList" :key="food.id" :food="food">
+      </Food>
+    </div>
+  </template>
   <v-snackbar v-model="snackbar" :timeout="5000" color="red accent-1" location="top">
     {{ snackbarMsg }}
   </v-snackbar>
---
+++ src/components/food/NoFood.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-row justify="center">
+        <v-col cols="md-8">
+            <v-card class="mx-auto">
+                <v-card-item>
+                    <v-row justify="center" align="center">
+                        <v-col cols="12" class="d-flex justify-center">
+                            <v-img src="/empty.jpg" height="300px" contain>
+                            </v-img>
+                        </v-col>
+                        <v-col cols="12" class="d-flex justify-center">
+                            <h3>There isn't any pizger!</h3>
+                        </v-col>
+                        <v-col cols="12" class="d-flex justify-center">
+                            <router-link to="/food">
+                                <v-btn variant="outlined" rounded="pill">
+                                    add pizger
+                                </v-btn>
+                            </router-link>
+                        </v-col>
+                    </v-row>
+                </v-card-item>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
