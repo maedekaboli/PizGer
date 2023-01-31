45d2cf9a691c87ed3a4587e6797a83cf07070f85
maede kaboli <kabolimaede@gmail.com>
2023-01-31 17:29:07 +0330

    some imporovment of ux in delete modal

--- src/views/food/DeleteModal.vue
+++ src/views/food/DeleteModal.vue
@@ -17,30 +17,39 @@ const close = () => {
 </script>
 
 <template>
-    <v-dialog :persistent="persistent" v-model="showModal" transition="dialog-bottom-transition">
-        <v-row justify="center">
-            <v-col cols="md-5">
-                <v-card>
-                    <v-progress-linear color="#6200ee" indeterminate v-if="modalLoading"></v-progress-linear>
-                    <v-img src="/modalBg.jpg" class="align-end" gradient=" rgba(0,0,0,.5), rgba(0,0,0,.5)"
-                        height="200px" cover>
-                        <v-sheet color="#ffffffd6"  height="200px">
-                            <v-card-text>
-                                <div class="text-h6">
-                                    <slot name="text"></slot>
-                                </div>
-                            </v-card-text>
-                            <v-card-actions class="justify-end">
-                                <v-btn :disabled="modalLoading" color="primary" variant="flat" rounded="pill"
-                                    @click="onAgree">
-                                    Agree
-                                </v-btn>
-                                <v-btn rounded="pill" variant="outlined" @click="close">Close</v-btn>
-                            </v-card-actions>
-                        </v-sheet>
-                    </v-img>
-                </v-card>
-            </v-col>
-        </v-row>
+    <v-dialog :persistent="persistent" v-model="showModal" transition="dialog-bottom-transition" maxWidth="500px">
+        <!-- <v-row justify="center"> -->
+        <!-- <v-col cols="md-5"> -->
+        <v-card class="bg-img" height="200px">
+            <v-sheet height="200px" class="d-flex flex-wrap" color="#ffffffd6">
+                <v-progress-linear color="#6200ee" indeterminate v-if="modalLoading"></v-progress-linear>
+                <v-card-text>
+                    <div class="text-h6">
+                        <slot name="text"></slot>
+                    </div>
+                </v-card-text>
+                <v-card-actions class="justify-end">
+                    <v-btn :disabled="modalLoading" color="primary" variant="flat" rounded="pill" @click="onAgree">
+                        Agree
+                    </v-btn>
+                    <v-btn rounded="pill" variant="outlined" @click="close">Close</v-btn>
+                </v-card-actions>
+            </v-sheet>
+        </v-card>
+        <!-- </v-col> -->
+        <!-- </v-row> -->
     </v-dialog>
 </template>
+
+<style scoped>
+.v-sheet {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.bg-img {
+    background-image: url('/modalBg.jpg');
+    background-position: center;
+    background-size: cover;
+}
+</style>
