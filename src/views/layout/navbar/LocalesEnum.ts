212cbfd7d4c2b1529a7123ae88a438ce57579b42
maede kaboli <kabolimaede@gmail.com>
2023-02-12 17:26:18 +0330

    set new locale functionality added but doesnt work in sidebar

---
+++ src/views/layout/navbar/LocalesEnum.ts
@@ -0,0 +1,5 @@
+enum LocaleEnum {
+    en, de
+}
+
+export default LocaleEnum
--- src/views/layout/navbar/LocalesMenu.vue
+++ src/views/layout/navbar/LocalesMenu.vue
@@ -1,5 +1,25 @@
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import { ref } from 'vue'
+import LocaleEnum from './LocalesEnum'
 
+const { locale } = useI18n()
+const menuItems = ref([
+    {
+        prependAvatar: '/en.png',
+        title: 'English',
+        value: LocaleEnum['en']
+    },
+    {
+        prependAvatar: '/de.png',
+        title: 'Deutschland',
+        value: LocaleEnum['de']
+    }
+])
+
+const changeLocale = (val: number) => {
+    locale.value = LocaleEnum[val]
+}
 </script>
 
 <template>
@@ -9,12 +29,10 @@
                 <v-img src="/de.png" alt="John"></v-img>
             </v-avatar>
         </template>
-        <v-list :items="[
-        {
-            prependAvatar: '/en.png',
-            title: 'English',
-            value: 0
-        }]" item-props lines="three">
+        <v-list active-color="primary" item-props lines="three">
+            <v-list-item v-for="(item, index) in menuItems" :key="index" @click="changeLocale(item.value)">
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
         </v-list>
     </v-menu>
 </template>
