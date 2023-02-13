f73253161a083cf8bf28c07f53c622a190dd8ece
maede kaboli <kabolimaede@gmail.com>
2023-02-13 11:09:42 +0330

    current locales img issued fixed

--- src/views/layout/navbar/LocalesMenu.vue
+++ src/views/layout/navbar/LocalesMenu.vue
@@ -5,33 +5,36 @@ import LocaleEnum from './LocalesEnum'
 import LocalesMenuModel from './LocalesMenuModel'
 
 const { locale } = useI18n()
+const currentLocaleImg = ref(locale.value)
 const menuItems = ref<LocalesMenuModel[]>([
     {
-        prependAvatar: '/en.png',
+        prependAvatar: 'en',
         title: 'English',
         value: LocaleEnum['en']
     },
     {
-        prependAvatar: '/de.png',
+        prependAvatar: 'de',
         title: 'Deutschland',
         value: LocaleEnum['de']
     }
 ])
 
-const changeLocale = (val: number) => {
-    locale.value = LocaleEnum[val]
+const changeLocale = (val: LocalesMenuModel) => {
+    locale.value = LocaleEnum[val.value]
+    currentLocaleImg.value = val.prependAvatar
 }
+console.log(locale.value)
 </script>
 
 <template>
     <v-menu>
         <template v-slot:activator="{ props }">
             <v-avatar class="ml-3" size="small" v-bind="props">
-                <v-img src="/de.png" alt="John"></v-img>
+                <v-img :src="`/${currentLocaleImg}.png`" alt="John"></v-img>
             </v-avatar>
         </template>
         <v-list active-color="primary" item-props lines="three">
-            <v-list-item v-for="(item, index) in menuItems" :key="index" @click="changeLocale(item.value)">
+            <v-list-item v-for="(item, index) in menuItems" :key="index" @click="changeLocale(item)">
                 <v-list-item-title>{{ item.title }}</v-list-item-title>
             </v-list-item>
         </v-list>
