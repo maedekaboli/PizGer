d600a5a247e7685e9beacf0551ddea2ac9dc1e13
maede kaboli <kabolimaede@gmail.com>
2023-02-13 11:32:13 +0330

    style of locales menu finished

--- src/views/layout/navbar/LocalesMenu.vue
+++ src/views/layout/navbar/LocalesMenu.vue
@@ -23,20 +23,31 @@ const changeLocale = (val: LocalesMenuModel) => {
     locale.value = LocaleEnum[val.value]
     currentLocaleImg.value = val.prependAvatar
 }
-console.log(locale.value)
 </script>
 
 <template>
     <v-menu>
         <template v-slot:activator="{ props }">
-            <v-avatar class="ml-3" size="small" v-bind="props">
+            <v-avatar class="ml-3 cursor-pointer" size="small" v-bind="props">
                 <v-img :src="`/${currentLocaleImg}.png`" alt="John"></v-img>
             </v-avatar>
         </template>
-        <v-list active-color="primary" item-props lines="three">
+        <v-list class="py-0" active-color="primary" item-props lines="three">
             <v-list-item v-for="(item, index) in menuItems" :key="index" @click="changeLocale(item)">
+                <template v-slot:prepend>
+                    <v-avatar size="x-small" color="primary">
+                        <v-img :src="`/${item.prependAvatar}.png`"></v-img>
+                    </v-avatar>
+                </template>
                 <v-list-item-title>{{ item.title }}</v-list-item-title>
             </v-list-item>
         </v-list>
     </v-menu>
 </template>
+
+
+<style scoped>
+.v-list-item--density-default:not(.v-list-item--nav).v-list-item--three-line {
+    min-height: 58px;
+}
+</style>
